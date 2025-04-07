6df84e070bd59863bb1f36c94348bf922d37ed11
snehlatakvs29@gmail.com <snehlatakvs29@gamil.com>
2025-04-07 17:59:15 +0530

    validate signup api

--- src/middlewares/app.js
+++ src/middlewares/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connectDB = require('../config/data_base'); 
 const User = require('../models/user');
+const { validateSignupData } = require('../utils/validation');
+const bcrypt = require('bcrypt');
+
 
 const app = express();
 app.use(express.json());
@@ -8,13 +11,25 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
  try{
+   validateSignupData(req);
+
+   const{ firstName, lastName, emailId, password} = req.body;
+
+   const passwordHash = await bcrypt.hash(password, 10);
+   console.log("Hashed Password:", passwordHash);
+
     console.log("Received Data:", req.body);
-    const user = new User(req.body);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
     console.log("Saved Data:", user);
     res.send("User Created Successfully");
  }catch(err){
-    res.status(400).send(err);
+    res.status(400).send("ERROR: " + err);
  }                                                                              
 });
 
@@ -42,6 +57,7 @@ app.get("/user", async (req, res) => {
 
    const users = await User.find({emailId:userEmail});
    try{
+
    if(users.length === 0){
      res.status(404).send("User not found");
    }
@@ -67,24 +83,66 @@ app.delete("/user", async (req, res) => {
 }
 });
 
-app.patch("/user", async (req, res) => {
-   const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => { 
+   const userId = req.params.userId;
    const data = req.body;
-   console.log(data);
+   console.log("Updated Data: ", data);
    try{
-      const user = await User.findByIdAndUpdate({_id:userId}, data,{
-         returnDocument: "after",
+      const ALLOWED_UPDATE = [
+         "userId",
+         "photoURL",
+         "password",
+         "about",
+         "gender",
+         "dob",
+         "age",
+         "skills",
+      ];
+      const isUpdateAllowed = Object.keys(data).every((k)=>
+         ALLOWED_UPDATE.includes(k)
+      );
+      if(!isUpdateAllowed){
+         res.status(400).send("Update not Allowed");
+      }
+
+      if (data.password) {
+         data.password = await bcrypt.hash(data.password, 10);
+      }
+
+      const user = await User.findByIdAndUpdate(userId, data,{
+         new: true,
+         runValidators: true,
       });
+
       console.log(user);
+
       if(!user){
          res.status(404).send("User not found");
-   }else{
-         res.send("User updated successfully");
       }
+      const formattedUser = {
+         _id: user._id,
+         firstName: user.firstName,
+         lastName: user.lastName,
+         emailId: user.emailId,
+         password: user.password,
+         dob: user.dob,
+         gender: user.gender,
+         profilePic: user.profilePic,
+         bio: user.bio,
+         isVerified: user.isVerified,
+         isDeleted: user.isDeleted,
+         isBlocked: user.isBlocked,
+         createdAt: user.createdAt,
+         updatedAt: user.updatedAt,
+         __v: user.__v,
+      };
+
+      res.send(formattedUser);
+
    }catch(err){   
-      res.status(400).send("something went wrong");
+      res.status(400).send("Update fail " + err.message);
    }
-})
+});
 
 app.get("/feed", async (req, res) => {
   try{
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,106 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
+        required:true,
+        minLength:3,
+        maxLength:50,
     },
     lastName:{
         type: String,
+        maxLength:50,
     },
     emailId:{
         type:String,
+        lowercase:true,
+        required:true,
+        unique:true,
+        trim:true,
+        validator: function(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email Id");
+            }
+        }
     },
     password:{
         type: String,
-    } 
+        required:true,
+        validator:function(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Password is not strong enough");
+            }
+        }
+    },
+    dob:{   
+        type: Date,
+        required:true,
+        validate: function(value){
+            if(!validator.isDate(value)){
+                throw new Error("Invalid Date of Birth");
+            }
+        }
+    }, 
+    gender: {
+        type: String,
+        required:true,
+        validate(value){
+          if(!["male","female","other"].includes(value)){
+            throw new Error("not valid gender");
+          }
+        }
+    },
+    profilePic:{
+        type: String,
+        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-978409_1280.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL for profile picture");
+            }
+        }
+    },
+    bio:{
+        type: String,
+        maxLength: 500,
+        default:"Hey there! I am using this app",
+        validate(value){
+            if(!validator.isLength(value, { max: 500 })){
+                throw new Error("Bio should be less than 500 characters");
+            }
+        }
+    },
+    phone:{
+        type: String,
+        validate(value){
+            if(!validator.isMobilePhone(value, 'any', { strictMode: false })){
+                throw new Error("Invalid Phone Number");
+            }
+        }
+    },
+    address:{
+        type: String,
+        validate(value){
+            if(!validator.isLength(value, { max: 100 })){
+                throw new Error("Address should be less than 100 characters");
+            }
+        }
+    },  
+    isVerified:{
+        type: Boolean,
+        default: false,
+    },
+    isDeleted:{
+        type: Boolean,
+        default: false,
+    },
+    isBlocked:{
+        type: Boolean,
+        default: false,
+    }
+},
+{
+    timestamps:true,
 }
 );
 
